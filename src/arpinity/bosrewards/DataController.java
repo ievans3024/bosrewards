bb647ed50782aa669ce51ad7ea0f8c0342e0aff5
ievans3024 <ievans3024@gmail.com>
2013-06-17 16:20:59 -0700

    + added database classes that do nothing

---
+++ src/arpinity/bosrewards/DataController.java
@@ -0,0 +1,26 @@
+package arpinity.bosrewards;
+
+public class DataController {
+	//TODO: Create code that manages plugin data (users and rewards catalogue)
+	// Flatfiles should be handled with config api
+	
+	//TODO: Create database API for the rest of the plugin to interface with
+	//Determine what kind of database is being used
+	//Determine what config options (if applicable) should be used
+	//Construct a database object that has generic external methods that use
+	//		the methods and connectivity specific to that database type.
+	//In theory, all this allows us to just add database types as we need to support them.
+	
+	
+	//"Get data"
+	// user
+	// reward
+	
+	
+	//"Set data"
+	// user
+	// reward
+	
+	
+	
+}
---
+++ src/arpinity/bosrewards/DataModels.java
@@ -0,0 +1,13 @@
+package arpinity.bosrewards;
+
+public class DataModels {
+	//TODO: Create data models for users and rewards
+	
+	public abstract class User {
+		//Fields
+	}
+	
+	public abstract class Reward {
+		//Fields		
+	}
+}
