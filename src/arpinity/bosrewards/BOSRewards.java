27711e201226bbad1a260fd1b94a26891289077b
ievans3024 <ievans3024@gmail.com>
2013-06-17 16:20:22 -0700

    + added plugin information to plugin.yml + added onEnable, onDisable, and reload command + added default config.yml file.

--- src/arpinity/bosrewards/BOSRewards.java
+++ src/arpinity/bosrewards/BOSRewards.java
@@ -1,13 +1,37 @@
 package arpinity.bosrewards;
 
-public class BOSRewards {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public final class BOSRewards extends JavaPlugin {
+	
+	public static final int testing = 0;
+	 
+    @Override
+    public void onEnable(){
+    	this.saveDefaultConfig();
+    	getLogger().info("BOSRewards Enabled!");
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+ 
+    @Override
+    public void onDisable() {
+    	getLogger().info("BOSRewards Disabled!");
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+    
+    
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("rewards")
+    			|| cmd.getName().equalsIgnoreCase("rw")
+    			|| cmd.getName().equalsIgnoreCase("bosrewards")){
+    		if (args[0].equalsIgnoreCase("reload")){
+    			getLogger().info("BOSRewards Reloaded!");
+        		//TODO: create reloadConfig() code	
+    		}
+    	}
+    	return true;
+    }
 
 }
