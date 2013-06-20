800548d9215408784516cf309a07a9d4044b4a7a
ievans3024 <ievans3024@gmail.com>
2013-06-20 13:09:56 -0700

    * changed public fields to private + added getters and setters

--- src/arpinity/bosrewards/Reward.java
+++ src/arpinity/bosrewards/Reward.java
@@ -3,8 +3,46 @@ package arpinity.bosrewards;
 import java.util.List;
 
 public class Reward {
-	public String id;
-	public String summary;
-	public int cost;
-	public List<String> commands;
+	private String id;
+	private String summary;
+	private int cost;
+	private List<String> commands;
+	
+	
+	// Getters
+
+	public final String getId(){
+		return id;
+	}
+	public final String getSummary(){
+		return summary;
+	}
+	public final int getCost(){
+		return cost;
+	}
+	public final List<String> getCommands(){
+		return commands;
+	
+	}
+	
+	//Setters
+	
+	public final void setId(String id){
+		this.id = id;
+	}
+	public final void setSummary(String summary){
+		this.summary = summary;
+	}
+	public final void setCost(int cost){
+		this.cost = cost;
+	}
+	public final void setFirstCommand(String command){
+		this.commands.set(0,command);
+	}
+	public final void setCommands(List<String> commands){
+		this.commands = commands;
+	}
+	public final void addCommands(String command){
+		this.commands.add(command);
+	}
 }
--- src/arpinity/bosrewards/User.java
+++ src/arpinity/bosrewards/User.java
@@ -1,6 +1,30 @@
 package arpinity.bosrewards;
 
 public class User {
-	public String name;
-	public int points;
+	private String name;
+	private int points;
+	
+	//Getters
+	
+	public final String getName(){
+		return this.name;
+	}
+	public final int getPoints(){
+		return this.points;
+	}
+	
+	//Setters
+	
+	public final void setName(String name){
+		this.name = name;
+	}
+	public final void setPoints(int points){
+		this.points = points;
+	}
+	public final void addPoints(int points){
+		this.points += points;
+	}
+	public final void subtractPoints(int points){
+		this.points -= points;
+	}
 }
