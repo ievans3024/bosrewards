616e2cfbd8f80e2b718d063b839dfb088d930a5d
ievans3024 <ievans3024@gmail.com>
2013-06-21 16:52:38 -0700

    + added reference material

--- src/arpinity/bosrewards/SubCommand.java
+++ src/arpinity/bosrewards/SubCommand.java
@@ -1,5 +1,6 @@
 package arpinity.bosrewards;
 
 public final class SubCommand {
-	//TODO: create better subcommand handling
+	// TODO: create better subcommand handling
+	// study how mondochest did it: https://bitbucket.org/crast/mondochest/src/612057ed44a1cfbbf307e39e116ffeebef3b4ca5/src/us/crast/mondochest/command/SubCommand.java?at=default
 }
