831fdb9c5f4206081217424d66725d8e415ac121
ievans3024 <ievans3024@gmail.com>
2013-06-25 17:40:02 -0700

    + added permissions nodes to plugin.yml + added permissions requirements to: 	/rewards balance <user> 	/rewards history <user> * changed history subcommand output logic

--- src/arpinity/bosrewards/subcommands/BalanceCommand.java
+++ src/arpinity/bosrewards/subcommands/BalanceCommand.java
@@ -2,6 +2,7 @@ package arpinity.bosrewards.subcommands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import arpinity.bosrewards.main.BOSRewards;
@@ -23,11 +24,16 @@ public final class BalanceCommand extends SubCommand {
 		String sentencePrefix;
 		
 		if (args.length > 0) {
-			if (this.getPlugin().getDataController().getUserExists(args[0])) {
-				user = this.getPlugin().getDataController().getUserByName(args[0]);
-				sentencePrefix = Messages.COLOR_INFO + user.getName() + " has ";
+			if (sender instanceof ConsoleCommandSender || sender.hasPermission("BOSRewards.admin.seebalance")) {
+				if (this.getPlugin().getDataController().getUserExists(args[0])) {
+					user = this.getPlugin().getDataController().getUserByName(args[0]);
+					sentencePrefix = Messages.COLOR_INFO + user.getName() + " has ";
+				} else {
+					sender.sendMessage(Messages.INVALID_ARGUMENT + args[0] + " not in rewards database");
+					return true;
+				}
 			} else {
-				sender.sendMessage(Messages.INVALID_ARGUMENT + args[0] + " not in rewards database");
+				Messages.sendNoPermsError(sender);
 				return true;
 			}
 		} else {
--- src/arpinity/bosrewards/subcommands/HistoryCommand.java
+++ src/arpinity/bosrewards/subcommands/HistoryCommand.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 import arpinity.bosrewards.main.BOSRewards;
@@ -23,17 +24,22 @@ public final class HistoryCommand extends SubCommand {
 		User user;
 		String header;
 		int pageNumber = 1;
-		if (args.length > 1){
-			if (this.getPlugin().getDataController().getUserExists(args[1])){
-				user = this.getPlugin().getDataController().getUserByName(args[1]);
-				header =  Messages.COLOR_INFO + "Redemption history for " + user.getName();
-				if (args.length > 2){
-					pageNumber = Integer.parseInt(args[2]);
+		if (args.length > 0){
+			if (this.getPlugin().getDataController().getUserExists(args[0])){
+				if (sender instanceof ConsoleCommandSender || sender.hasPermission("BOSRewards.admin.seehistory")) {
+					user = this.getPlugin().getDataController().getUserByName(args[0]);
+					header =  Messages.COLOR_INFO + "Redemption history for " + user.getName();
+					if (args.length > 2){
+						pageNumber = Integer.parseInt(args[1]);
+					}
+				} else {
+					Messages.sendNoPermsError(sender);
+					return true;
 				}
 			} else if (sender instanceof Player){
 				user = this.getPlugin().getDataController().getUserByName(sender.getName());
 				header = Messages.COLOR_INFO + "Your redemption history: ";
-				pageNumber = Integer.parseInt(args[1]);
+				pageNumber = Integer.parseInt(args[0]);
 			} else {
 				Messages.sendNoPermsError(sender);
 				return true;
@@ -48,32 +54,36 @@ public final class HistoryCommand extends SubCommand {
 			}
 		}
 		List<String> userReceipts = user.getReceipts();
+		String[] message;
 		if (!userReceipts.isEmpty()){
 			int maximumPages = userReceipts.size() / 5;
 			if (maximumPages % 5 != 0){
 				maximumPages += 1;
 			}
 			if (pageNumber > maximumPages){
-				String message = Messages.COLOR_SYNTAX_ERROR + "There are not that many pages. There are " + maximumPages + ((maximumPages == 1) ? " page." : " pages.");
-				sender.sendMessage(message);
-				return true;
-			}				
-			int listStart = (pageNumber * 5) - 5;
-			String[] message = new String[7];
-			message[0] = header;
-			message[1] = "--------------------------";
-			int i = 0;
-			int m = 2;
-			while (m < 7){
-				message[m] = Messages.COLOR_INFO + userReceipts.get(listStart + i);
-				m++;
-				i++;
-				if ((listStart + i) > (userReceipts.size() - 1)){
-					m = 7;
+				message = new String[1];
+				message[0] = Messages.COLOR_SYNTAX_ERROR + "There are not that many pages. There are " + maximumPages + ((maximumPages == 1) ? " page." : " pages.");
+			} else {				
+				int listStart = (pageNumber * 5) - 5;
+				message = new String[7];
+				message[0] = header;
+				message[1] = "--------------------------";
+				int i = 0;
+				int m = 2;
+				while (m < 7){
+					message[m] = Messages.COLOR_INFO + userReceipts.get(listStart + i);
+					m++;
+					i++;
+					if ((listStart + i) > (userReceipts.size() - 1)){
+						m = 7;
+					}
 				}
 			}
-			sender.sendMessage(message);
+		} else {
+			message = new String[1];
+			message[0] = Messages.COLOR_INFO + "No rewards purchased yet!";
 		}
+		sender.sendMessage(message);
 		return true;
 	}
 
