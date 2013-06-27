d8a81c11e35bd4ab60ed69f8d76c113926a22fd7
ievans3024 <ievans3024@gmail.com>
2013-06-26 17:27:45 -0700

    + added rewards/user table saver task class

---
+++ src/arpinity/bosrewards/main/SaveTask.java
@@ -0,0 +1,19 @@
+package arpinity.bosrewards.main;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public final class SaveTask extends BukkitRunnable {
+	
+	private final BOSRewards plugin;
+	
+	public SaveTask(BOSRewards plugin) {
+		this.plugin = plugin;
+	}
+	
+	public void run() {
+		plugin.getLogger().info("Saving user and rewards tables...");
+		plugin.getDataController().writeRewards();
+		plugin.getDataController().writeUsers();
+		plugin.getLogger().info("User and rewards tables saved!");
+	}
+}
